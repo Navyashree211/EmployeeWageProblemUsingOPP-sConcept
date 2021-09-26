b6d161c38fdeec2d9ea4104cfecd018d8867849f
navyas211@gmail.com <navyas211@gmail.com>
2021-09-26 22:29:01 +0530

    [NAVYA]ADDED: Welcome to employee wage problem using opp's concept

---
+++ src/com/bridgelabz/employeewage/program/WelcomeEmpWageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewage.program;
+
+public class WelcomeEmpWageComputation {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage Computation Problem Using OPP's Concept");
+
+	}
+
+}
